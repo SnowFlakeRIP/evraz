997530600100dae5912a1b4fc262d39c7f30947e
Shmakov Alexander <schmakov8@gmail.com>
2023-02-18 13:12:32 +0500

    f

--- index.js
+++ index.js
@@ -178,9 +178,29 @@ fastify.post('/film/create',async(request,reply) => {
         statusCode:400
     }
     const client = await pool.connect()
+    const object = request.body
     try{
-    
-    
+        const createFilm = await client.query(`insert into films ("posterUrlPreview", "nameRu", "nameOriginal",
+                                                                  "ratingImdb", "ratingKinopoisk", "type", "endYear",
+                                                                  "startYear", "filmLength", description, countries,
+                                                                  genres)
+                                               values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,
+                                                       $12) returning "kinopoiskId"::integer`, [object.posterUrlPreview, object.nameRu, object.nameOriginal, object.ratingImdb, object.ratingKinopoisk, object.type, object.endYear, object.startYear, object.filmLength, object.description, object.countries, object.genres])
+
+        if(createFilm.rowCount > 0 && createFilm.rows.length > 0){
+            data.message = {
+                success: true,
+                kinopoiskId:createFilm.rows[0].kinopoiskId
+            }
+        }
+        else{
+            console.log(`Ошибка при создании фильма`)
+
+            data.message = {
+                success: false
+            }
+            data.statusCode = 400
+        }
     }
     catch (e) {
         console.log(e);
@@ -188,6 +208,104 @@ fastify.post('/film/create',async(request,reply) => {
     finally {
         client.release()
     }
+    reply.send(data)
+})
+
+fastify.get('/film/show',async (request,reply)=>{
+    let data = {
+        message:'error',
+        statusCode:400
+    }
+    const client = await pool.connect()
+    const object = request.body
+    try{
+        const selectFilms = await client.query(`select * from films`)
+        data.message = selectFilms.rows
+        data.statusCode = 200
+    }
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+        client.release()
+    }
+    reply.send(data)
+})
+
+fastify.post('/film/update',async (request,reply)=>{
+    let data = {
+        message:'error',
+        statusCode:400
+    }
+    const client = await pool.connect()
+    const object = request.body
+    try{
+        const updateFilm = await client.query(`update films
+                                               set "posterUrlPreview" = $1,
+                                                   "nameRu"           = $2,
+                                                   "nameOriginal"     = $3,
+                                                   "ratingImdb"       = $4,
+                                                   "ratingKinopoisk"  = $5,
+                                                   "type"             = $6,
+                                                   "endYear"          = $7,
+                                                   "startYear"        = $8,
+                                                   "filmLength"       = $9,
+                                                   "description"      = $10,
+                                                   "countries"        = $11,
+                                                   "genres"           = $12
+                                               where "kinopoiskId" = $13`, [object.posterUrlPreview, object.nameRu, object.nameOriginal, object.ratingImdb, object.ratingKinopoisk, object.type, object.endYear, object.startYear, object.filmLength, object.description, object.countries, object.genres, object.kinopoiskId])
+        if(updateFilm.rowCount > 0){
+            data.message = {
+                success:true
+            }
+            data.statusCode = 200
+        }
+        else{
+            data.message = {
+                success:false
+            }
+            data.statusCode = 400
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+        client.release()
+    }
+    reply.send(data)
+})
+
+fastify.post('/film/remove', async (request, reply) => {
+    let data = {
+        message: 'error',
+        statusCode: 400
+    }
+    const client = await pool.connect()
+    const object = request.body
+    try {
+        const removeFilm = await client.query(`delete from films where "kinopoiskId" = $1`,[object.kinopoiskId])
+        if(removeFilm.rowCount > 0){
+            data.message = {
+                success:true
+            }
+            data.statusCode = 200
+        }
+        else{
+            data.message = {
+                success:false
+            }
+            data.statusCode = 400
+
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+        client.release()
+    }
+    reply.send(data)
 })
 
 fastify.listen(({port:5000,host:'0.0.0.0'}), (err, address) => {
